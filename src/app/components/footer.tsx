6f55d462add2f6141cf761ea3517f2d1195585b3
Mathias Olesen <math09s0@stud.kea.dk>
2024-11-25 19:04:23 +0100

    init

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,35 @@
+import styles from "@/app/scss/footer.module.scss"
+import Image from "next/image"
+
 export default function Footer() {
     return(
-        <>
-        </>
+        <nav className={styles.container_1}>
+            <div className={styles.container_1_1}>
+                <ul>
+                    <li>FAQ</li>
+                    <li>Return policy</li>
+                    <li>Shipping</li>
+                    <li>Terms & conditions</li>
+                    <li>Privacy policy</li>
+                    <li>Accessibility</li>
+                </ul>
+            </div>
+            <div className={styles.container_1_2}>
+                <Image
+                src="/logo_nijicorp_blanc.png"
+                alt=""
+                width={200}
+                height={60}
+                />
+            </div>
+            <div className={styles.container_1_3}>
+                <ul>
+                    <li>Contact</li>
+                    <li>Instagram</li>
+                    <li>Facebook</li>
+                    <li>TikTok</li>
+                </ul>
+            </div>
+        </nav>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "@/app/page.module.scss"
 
+
 export default function Home() {
   return (
    <main className={styles.main_container}>
+    <div className={styles.header_background}></div>
     <div className={styles.container_1}>
       <Image className={styles.Background_image}
       src="https://cdn.shopify.com/s/files/1/0896/4272/9819/files/landingpage-image.jpg?v=1732368446"
@@ -13,8 +15,49 @@ export default function Home() {
     </div>
     <section className={styles.container_2}>
       <h1>Signature collection</h1>
-      
+      <div className={styles.container_2_1}>
+        <div className={styles.container_2_1_1}>
+          <div className={styles.image_container}>
+          <Image
+          src="/Mannequin_Image1 2.png"
+          alt=""
+          width={400}
+          height={400}
+          />
+          </div>
+          <div className={styles.product_text}>
+            <p>Signature Star Tee</p>
+            <p>500 DKK</p>
+          </div>
+        </div>
+        <div className={styles.container_2_1_2}>
+          <div className={styles.image_container}>
+          <Image
+          src="/Mannequin_Image1.png"
+          alt=""
+          width={400}
+          height={400}
+          />
+          </div>
+          <div className={styles.product_text}>
+            <p>Signature Star Tee</p>
+            <p>500 DKK</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className={styles.container_3}>
+       <div className={styles.container_3_1}>
+        <div className={styles.container_3_1_1}>
 
+        </div>
+        <div className={styles.container_3_1_2}>
+
+        </div>
+       </div>
+       <div className={styles.container_3_2}>
+
+       </div>
     </section>
 
 
